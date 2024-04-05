cd834e822e890115b13074add55145fb9453a73e
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-04-05 18:39:39 -0500

    fixed it

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  baseURL: '/meal-search/',
+  base: '/meal-search/',
   plugins: [vue()],
   resolve: {
     alias: {
